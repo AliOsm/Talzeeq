c21e3942d013851ffb387c1af084e9b2aeab0775
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2021-01-01 16:13:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Talzeeq
+
+`PyQt5` based desktop application to build Deep Learning models in a drag-and-drop environment.
---
+++ main.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+
+import sys
+
+from PyQt5 import QtWidgets
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    sys.exit(app.exec_())
